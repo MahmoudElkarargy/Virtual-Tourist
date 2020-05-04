0ca2e1f9d64b756e76cd48e6b0964d36564c92bb
Mahmoud Elkarargy <mahmoud.elkarargy@gmail.com>
2020-05-04 20:01:31 +0200

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Mahmoud Elkarargy on 5/4/20.
+//  Copyright © 2020 Mahmoud Elkarargy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
